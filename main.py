af8ce18a40a915f605b01020fa0fe2a61c02d1f1
Sadeep Weerasinghe <darshanasadeep@gmail.com>
2020-05-24 21:06:38 +0530

    replaced the matplotlib plotting with seaborn

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from pathlib import Path
 from subprocess import call
 from keras.models import load_model
 from itertools import groupby
+import seaborn as sns; sns.set()
+import pandas as pd
 
 env = {}
 _normalized_frequencies_for_file_cache = {}
@@ -141,58 +143,77 @@ def auto_train(model_info, data_files_info, epochs, plot, new_model=False, reloa
         model = load_model("./models/"+use_model)
         env['id'] = use_model + "_" + str(int(time.time() * 1000))
 
-    fig = plt.figure()
     for i in epochs:
 
         if i == 'm':
             save_model(epoch_count)
             continue
 
-        caption = "epoch:"+str(epoch_count)+" reload:"+str(reload)#+" norm_ex:"+str(env['norm_ex'])
+        caption =  ("[" + "-".join([str(x) for x in model_info[0]]) + "]   ")+"epoch: "+str(epoch_count)
 
         if peek and  reload:
-            plot_files(files,data_files_info,model,model_info,plot_perc,"old_data "+caption)
+            plot_files(files, model, model_info, plot_perc, ("old_data " if reload else "") + caption)
         if reload:
             reload_n_process_data(data_files_info) # files, normalized_frequencies assigned
         if plot:
-            plot_files(files,data_files_info,model,model_info,plot_perc,"new_data "+caption)
-        if(i != 0):
+            plot_files(files, model, model_info, plot_perc, ("new_data " if reload else "") + caption)
+        if i != 0:
             train(model, normalized_frequencies, i)
             epoch_count+=i
 
     if plot:
-        caption = "epoch:" + str(epoch_count) + " reload:" + str(reload) + " norm_ex:" + str(env['norm_ex'])
-        plot_files(files,data_files_info,model,model_info,plot_perc,caption)
+        caption = ("[" + "-".join([str(x) for x in model_info[0]]) + "]   ") + "epoch: " + str(epoch_count)
+        plot_files(files, model, model_info, plot_perc, caption)
 
     save_video(env['id'])
     save_model()
 
 def delete_pngs():call("rm *.png", cwd="./figures", shell=True)
 
-def save_model(caption=""):model.save("./models/"+env['id']+" "+str(caption))
+
+def pd_vstack(dfs):
+    big_list = None
+    for i in dfs:
+        if big_list is None: big_list = i
+        else:big_list = big_list.append(i, ignore_index=True)
+    return big_list
+
+def save_model(caption=""):model.save("./models/"+env['id']+"_"+str(caption))
 
 def save_video(id):  call("ffmpeg -framerate 1 -i " + str(id) + "_%12d.png " + str(id) + ("_[" + "-".join([str(x) for x in model_info[0]]) + "]_") +str(int(time.time() * 1000))+ ".mp4", cwd="./figures", shell=True)
 
-def plot_files(files,data_files_info,model,model_info,perc = .1,caption = ""):
+def plot_files(files, model, model_info, perc = .1, caption =""):
     global photo_index
 
     mapping_function = K.function([model.layers[0].input], [model.layers[model_info[1]-1].output])
+
+    plot_dfs_list = []
+
     for file_index in range(len(files)):
         if(file_index not in _normalized_frequencies_for_file_cache):
             _normalized_frequencies_for_file_cache[file_index] = get_normalized_frequencies_for_file(files[file_index][0:int(len(files[file_index])*perc)])
         points = mapping_function(_normalized_frequencies_for_file_cache[file_index])[0]
-        X,Y = [i[0] for i in points], [i[1] for i in points]
-        plt.scatter(X, Y, color = data_files_info[file_index]['color'], s=scatter_point_size, alpha=.8)
+        X,Y,F = [i[0] for i in points], [i[1] for i in points], get_file_1st_names()[file_index]
+
+        plot_dfs_list.append(pd.DataFrame(         {'X': X,      'Y': Y,         'Sequence Name': F     }))
 
     Path("./figures").mkdir(parents=True, exist_ok=True)
-    plt.figtext(0.5, 0.01, caption, wrap=True, horizontalalignment='center', fontsize=12)
-    plt.savefig("figures/"+env['id'] +"_"+ str(photo_index).zfill(12))
+    plt.figure()
+    df = pd_vstack(plot_dfs_list).sample(frac=1).reset_index(drop=True)
+    ax = sns.scatterplot(x='X', y='Y', hue='Sequence Name', data=df, edgecolor="none",alpha=1,hue_order=get_file_1st_names())
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    ax.set_ylabel('')
+    ax.set_xlabel('')
+    ax.grid(False)
+    plt.title(caption)
+    plt.savefig("figures/"+env['id'] +"_"+ str(photo_index).zfill(12), bbox_inches='tight')
     photo_index +=1
-    plt.show(block=False)
+    plt.show()
 
 def sum_epochs(epo = None):
     if epo is None:epo =epochs
     return sum(x for x in epo if x != 'm')
+def get_file_1st_names():return [x['file'].split("/")[-1].split(".")[0] for x in data_files_info]
 
 def build_data_files_info(dir):
     len_mean = 5000
@@ -205,9 +226,9 @@ def build_data_files_info(dir):
 ########################################################################################################################
 
 data_files_info = [
-                # dict    (file ="data/bat_sars.fasta", len_mean = 5000, len_sd = 1000, count = 1000, color ='r'),
-                # dict    (file ="data/covid_19.fasta", len_mean = 5000, len_sd = 1000, count = 1000, color ='g'),
-                # dict    (file ="data/sars.fasta",     len_mean = 5000, len_sd = 1000, count = 1000, color ='b'),
+                dict    (file ="data/bat_sars.fasta", len_mean = 5000, len_sd = 1000, count = 1000, color ='r'),
+                dict    (file ="data/covid_19.fasta", len_mean = 5000, len_sd = 1000, count = 1000, color ='g'),
+                dict    (file ="data/sars.fasta",     len_mean = 5000, len_sd = 1000, count = 1000, color ='b'),
                 #
                 # dict(file="data/escherichia coli.fasta", len_mean=5000, len_sd=1000, count=1000, color='y'),
                 # dict(file="data/pseudomonas aeruginosa.fasta", len_mean=5000, len_sd=1000, count=1000, color='k'),
@@ -219,15 +240,15 @@ data_files_info = [
 
 model_info = (
                 #[32,16,6,2,6,16,32],
-                [32,16,2,16,32],      # layers
-                2               # autoencoder output layer index
+                [32,2,32],      # layers
+                1               # autoencoder output layer index
 )
 
 scatter_point_size = 8
 ########################################################################################################################
 
 
-data_files_info = build_data_files_info("data/21s")
+#data_files_info = build_data_files_info("data/21s")
 
 reload_n_process_data(data_files_info)
 
@@ -236,7 +257,7 @@ model = build_model(model_info)
 
 env['norm_ex'] = 1
 
-epochs = [0,4,10,6,'m']+[10]*8+[10]*30+[250]*50+['m']+[1000]*30+['m']+[1000]*20#+[10]*10+[4000]*3+['m']+[10]*10+['m']#+[50]*200
+epochs = [0,4,10,6,'m']+[10]*8+[10]*330+[250]*50+['m']+[1000]*30+['m']+[1000]*20#+[10]*10+[4000]*3+['m']+[10]*10+['m']#+[50]*200
 
 
-#auto_train(model_info, data_files_info, epochs=epochs,current_epoch=0, plot=True, new_model=True, reload=False, peek=True,plot_perc=.2)
+auto_train(model_info, data_files_info, epochs=epochs,current_epoch=0, plot=True, new_model=True, reload=False, peek=True,plot_perc=.2)
