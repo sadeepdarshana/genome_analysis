af71da94deb24f2855063c40538aa240a21d033a
Sadeep Weerasinghe <darshanasadeep@gmail.com>
2020-07-17 16:09:09 +0530

    Implemented metagenomic_counter. Bug fixes in pyVectorizer

---
+++ metagenomics_counter.py
@@ -0,0 +1,22 @@
+import json
+
+import numpy as np
+import pyVectorizer
+import pandas as pd
+
+def process(input_path, k, output_path):
+    vectors = np.array(pyVectorizer.vectorize_file(input_path, k)).astype(np.float32)
+    acgt = pyVectorizer.count_acgt_file(input_path)
+
+    vectorSum = np.zeros(len(pyVectorizer.all_kmers(k)),dtype=np.int64)
+
+    for i in vectors:
+        vectorSum += np.array(i).astype(np.int64)
+
+    with open(output_path+"_freqs.json", 'w') as outfile:
+        json.dump((k,vectorSum.tolist()), outfile)
+
+    with open(output_path+"_acgt.json", 'w') as outfile:
+        json.dump(acgt, outfile)
+
+t=process("./data/AAGA01.1.fsa_nt",3,'outtt')
--- pyVectorizer.py
+++ pyVectorizer.py
@@ -3,9 +3,9 @@ from collections import defaultdict
 from itertools import product
 
 all_kmers_cache = {}
-def all_kmers(k):
-    if k in all_kmers_cache: return all_kmers_cache[k]
-    def reverse_compliment(s):
+
+
+def reverse_compliment(s):
         reverse_letter = {
             'A': 'T',
             'C': 'G',
@@ -14,6 +14,8 @@ def all_kmers(k):
         }
         return ''.join([reverse_letter[i] for i in s[::-1]])
 
+def all_kmers(k):
+    if k in all_kmers_cache: return all_kmers_cache[k]
     all_strings = [''.join(i) for i in product("ACGT", repeat=k)]
     proper_strings = [min(i,reverse_compliment(i)) for i in all_strings]
     unique_kmers = list(set(proper_strings))
@@ -25,16 +27,32 @@ def vectorize_seq(seq, k=4):
     d = defaultdict(lambda: 0)
     for c in range(len(seq)-k+1):
         d[seq[c:c+k]]+=1
-    return [d[kmer] for kmer in all_kmers(k)]
+    return [d[kmer] if kmer==reverse_compliment(kmer) else d[kmer]+d[reverse_compliment(kmer)]for kmer in all_kmers(k)]
 
 def vectorize_file(path, k):
     file=SeqIO.parse(path, "fasta")
     current_seq=0
     vectors = []
     for i in file:
-        vectors.append(vectorize_seq(str(i.seq),k))
+        vec = vectorize_seq(str(i.seq), k)
+        vectors.append(vec)
         if not(current_seq%1000): print("Processing sequence "+str(current_seq))
         current_seq+=1
     return vectors
+
+def count_acgt_file(path):
+    file=SeqIO.parse(path, "fasta")
+    current_seq=0
+    acgt = [0,0,0,0]
+    for i in file:
+        seq = str(i.seq)
+        acgt[0] += (seq.count("A")+seq.count("a"))
+        acgt[1] += (seq.count("C")+seq.count("c"))
+        acgt[2] += (seq.count("G")+seq.count("g"))
+        acgt[3] += (seq.count("T")+seq.count("t"))
+
+        if not(current_seq%1000): print("Processing sequence "+str(current_seq))
+        current_seq+=1
+    return acgt
 
 #vectors = vectorize_file("./data/AAGA01.1.fsa_nt",4)
